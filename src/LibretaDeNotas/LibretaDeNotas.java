576fd0040121567c4b443e45e6a84941bf8da521
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-02-26 09:58:19 -0300

    Se agregan funciones para el promedio, notas mas altas y mas baja

--- src/LibretaDeNotas/LibretaDeNotas.java
+++ src/LibretaDeNotas/LibretaDeNotas.java
@@ -3,6 +3,8 @@ package LibretaDeNotas;
 import java.util.*;
 
 public class LibretaDeNotas {
+    //TODO: + 1 Realizar ingreso de cantidad de alumnos y notas. Ingresar nombre de alumnos y sus notas.
+
     static Map<String, List<Double>> calificacionEstudiante = new HashMap<>();
 
     public static double ingresoNotas(Scanner sc) {
@@ -10,14 +12,17 @@ public class LibretaDeNotas {
         do{
             System.out.println("Ingrese las notas del alumno: ");
             nota = sc.nextDouble();
-        }while(nota < 1 || nota > 7);
+        }while (nota < 1 || nota > 7);
         sc.nextLine();
         return nota;
     }
 
     public static int cantidad(String mensaje, Scanner sc) {
-        System.out.println(mensaje);
-        int cantidad = sc.nextInt();
+        int cantidad;
+        do{
+            System.out.println(mensaje);
+            cantidad = sc.nextInt();
+        } while (cantidad <= 0);
         sc.nextLine();
         return cantidad;
     }
@@ -47,9 +52,78 @@ public class LibretaDeNotas {
         }
     }
 
-    //TODO: 2 Realizar el calculo de las notas por alumno (Promedio).
-    //TODO: 3 Mostrar el promedio, la nota mas alta y la nota mas baja.
+    //TODO: + 2 Realizar el calculo de las notas por alumno (Promedio).
+
+    public static void promedioTodosAlumnos() {
+        double sumaTotal = 0;
+        int totalNotas = 0;
+        for(List<Double> notas : calificacionEstudiante.values()) {
+            for(Double nota : notas) {
+                sumaTotal = sumaTotal + nota;
+                totalNotas++;
+            }
+        }
+        double promedio = totalNotas > 0 ? sumaTotal / totalNotas : 0;
+        System.out.println("El promedio general del curso => " + promedio);
+    }
+
+    public static void promedioAlumno() {
+        String alumnoMasAlto = "";
+        String alumnoMasBajo = "";
+        double promedioMasAlto = Double.MIN_VALUE;
+        double promedioMasBajo = Double.MAX_VALUE;
+
+        for(Map.Entry<String, List<Double>> e : calificacionEstudiante.entrySet()) {
+            String alumno = e.getKey();
+            List<Double> notas = e.getValue();
+
+            double sumaNotas = 0;
+            for(Double nota : notas) {
+                sumaNotas = sumaNotas + nota;
+            }
+            double promedioAlumno = notas.size() > 0 ? sumaNotas / notas.size() : 0;
+            System.out.println("El promedio de " + alumno + " es: " + promedioAlumno);
+
+            if (promedioAlumno > promedioMasAlto) {
+                promedioMasAlto = promedioAlumno;
+                alumnoMasAlto = alumno;
+            }
+            if (promedioAlumno < promedioMasBajo) {
+                promedioMasBajo = promedioAlumno;
+                alumnoMasBajo = alumno;
+            }
+        }
+        System.out.println("\nEl alumno con el promedio más alto es: " + alumnoMasAlto + " con un promedio de: " + promedioMasAlto);
+        System.out.println("El alumno con el promedio más bajo es: " + alumnoMasBajo + " con un promedio de: " + promedioMasBajo);
+    }
+
+    //TODO: + 3 Mostrar el promedio, la nota mas alta y la nota mas baja.
+
+    public static void notaMasAltaYMasBajaEnTodoElCurso() {
+        // Variables para almacenar la nota más alta y la más baja
+        double notaMasAlta = Double.MIN_VALUE;
+        double notaMasBaja = Double.MAX_VALUE;
+
+        // Recorrer cada lista de notas de cada alumno
+        for (List<Double> notas : calificacionEstudiante.values()) {
+            // Recorrer las notas de cada alumno
+            for (Double nota : notas) {
+                // Encontrar la nota más alta
+                if (nota > notaMasAlta) {
+                    notaMasAlta = nota;
+                }
+                // Encontrar la nota más baja
+                if (nota < notaMasBaja) {
+                    notaMasBaja = nota;
+                }
+            }
+        }
+        System.out.println("La nota más alta en todo el curso es: " + notaMasAlta);
+        System.out.println("La nota más baja en todo el curso es: " + notaMasBaja);
+    }
+
     //TODO: 4 Hacer un menu.
+    //TODO: + 5 Validaciones: Cantidades de notas y/o alumnos deben ser un numero positivo.
 
     public static void main(String[] args) {
 
@@ -77,30 +151,9 @@ public class LibretaDeNotas {
         sc.close();
 
         mostrarDiccionario();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        promedioAlumno();
+        promedioTodosAlumnos();
+        notaMasAltaYMasBajaEnTodoElCurso();
 
 
     }
